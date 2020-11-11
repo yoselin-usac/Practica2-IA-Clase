a71d2e61ff04a53d8a4c19de2d6fe33f0c846ea0
yoselin-usac <67341591+yoselin-usac@users.noreply.github.com>
2020-11-11 09:51:57 -0600

    Codigo actualizado, estado subido a la mv

--- practica2.py
+++ practica2.py
@@ -1,33 +1,11 @@
-from flask import Flask, request, flash, url_for
-from flask_cors import CORS
-from markupsafe import escape
+from flask import Flask, request, render_template
 import random
+import operator
 
 # Configuraciones
 app = Flask(__name__)
-CORS(app)
-
-def op(turno):
-    if turno ==0:
-        return 1
-    else:
-        return 0
 
 # Rutas del api
-@app.route('/', methods=['GET'])
-def hello_world():
-    return "Hello, cross-origin-world!"
-
-@app.route('/grupo/<int:group_id>', methods=['GET'])
-def getgrupo(group_id):
-    return 'form_grupo ' + str(group_id)
-
-@app.route('/data', methods=['GET'])
-def data():
-    # here we want to get the value of user (i.e. ?user=some-value)
-    user = request.args.get('user')
-    return user
-
 @app.route('/random', methods=['GET'])
 def m_random():
     turno = request.args.get('turno')
@@ -49,111 +27,33 @@ def m_random():
             if matriz[x][y]==turno:
                 #norte
                 if x >1:
-                    if matriz[x-1][y] == op(turno):
-                        # print("norte")
-                        a=x-2
-                        while a>=0:
-                            if matriz[a][y]==2:
-                                mov.append(str(a)+str(y))
-                                break
-                            elif matriz[a][y]==turno:
-                                break
-                            a=a-1
-		        #sur
+                    norte(matriz, mov, turno, x, y)
+                #sur
                 if x<6:
-                    if matriz[x+1][y]==op(turno):
-                        # print("sur")
-                        a=x+2
-                        while a<=7:
-                            if matriz[a][y]==2:
-                                mov.append(str(a)+str(y))
-                                break
-                            elif matriz[a][y]==turno:
-                                break
-                            a=a+1
+                    sur(matriz, mov, turno, x, y)
                 #oeste
                 if y>1:
-                    if matriz[x][y-1]==op(turno):
-                        # print("oeste")
-                        a=y-2
-                        while a>=0:
-                            if matriz[x][a]==2:
-                                mov.append(str(x)+str(a))
-                                break
-                            elif matriz[x][a]==turno:
-                                break
-                            a=a-1
+                    oeste(matriz, mov, turno, x, y)
                 #este
                 if y<6:
-                    if matriz[x][y+1]==op(turno):
-                        # print("este")
-                        a=y+2
-                        while a<=7:
-                            if matriz[x][a]==2:
-                                mov.append(str(x)+str(a))
-                                break
-                            elif matriz[x][a]==turno:
-                                break
-                            a=a+1
+                    este(matriz, mov, turno, x, y)
                 #noroeste
                 if x>1 and y>1:
-                    if matriz[x-1][y-1]==op(turno):
-                        # print("noroeste")
-                        a = x - 2
-                        b = y - 2
-                        while a >= 0 and b >= 0:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a-1
-                            b=b-1
+                    noroeste(matriz, mov, turno, x, y)
                 #noreste
                 if x>1 and y<6:
-                    if matriz[x-1][y+1]==op(turno):
-                        # print("noreste")
-                        a=x-2
-                        b=y+2
-                        while a>=0 and b<=7:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a-1
-                            b=b-1
+                    noreste(matriz, mov, turno, x, y)
                 #suroeste
                 if x<6 and y>1:
-                    if matriz[x+1][y-1]==op(turno):
-                        # print("suroeste")
-                        a=x+2
-                        b=y-2
-                        while a<=7 and b>=0:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a+1
-                            b=b-1
+                    suroeste(matriz, mov, turno, x, y)
                 #sureste
                 if x<6 and y<6:
-                    if matriz[x+1][y+1]==op(turno):
-                        # print("sureste")
-                        a=x+2
-                        b=y+2
-                        while a<=7 and b<=7:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a+1
-                            b=b+1
+                    sureste(matriz, mov, turno, x, y)
     max = len(mov)-1
     indice = random.randint(0, max)
-    return str(mov[indice])
+    return render_template("index.html",
+                           mycontent=str(mov[indice]))
+
 
 @app.route('/heuristica', methods=['GET'])
 def m_heuristica():
@@ -176,135 +76,164 @@ def m_heuristica():
             if matriz[x][y]==turno:
                 #norte
                 if x >1:
-                    if matriz[x-1][y] == op(turno):
-                        # print("norte")
-                        a=x-2
-                        while a>=0:
-                            if matriz[a][y]==2:
-                                mov.append(str(a)+str(y))
-                                break
-                            elif matriz[a][y]==turno:
-                                break
-                            a=a-1
-		        #sur
+                    norte(matriz, mov, turno, x, y)
+                #sur
                 if x<6:
-                    if matriz[x+1][y]==op(turno):
-                        # print("sur")
-                        a=x+2
-                        while a<=7:
-                            if matriz[a][y]==2:
-                                mov.append(str(a)+str(y))
-                                break
-                            elif matriz[a][y]==turno:
-                                break
-                            a=a+1
+                    sur(matriz, mov, turno, x, y)
                 #oeste
                 if y>1:
-                    if matriz[x][y-1]==op(turno):
-                        # print("oeste")
-                        a=y-2
-                        while a>=0:
-                            if matriz[x][a]==2:
-                                mov.append(str(x)+str(a))
-                                break
-                            elif matriz[x][a]==turno:
-                                break
-                            a=a-1
+                    oeste(matriz, mov, turno, x, y)
                 #este
                 if y<6:
-                    if matriz[x][y+1]==op(turno):
-                        # print("este")
-                        a=y+2
-                        while a<=7:
-                            if matriz[x][a]==2:
-                                mov.append(str(x)+str(a))
-                                break
-                            elif matriz[x][a]==turno:
-                                break
-                            a=a+1
+                    este(matriz, mov, turno, x, y)
                 #noroeste
                 if x>1 and y>1:
-                    if matriz[x-1][y-1]==op(turno):
-                        # print("noroeste")
-                        a = x - 2
-                        b = y - 2
-                        while a >= 0 and b >= 0:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a-1
-                            b=b-1
+                    noroeste(matriz, mov, turno, x, y)
                 #noreste
                 if x>1 and y<6:
-                    if matriz[x-1][y+1]==op(turno):
-                        # print("noreste")
-                        a=x-2
-                        b=y+2
-                        while a>=0 and b<=7:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a-1
-                            b=b-1
+                    noreste(matriz, mov, turno, x, y)
                 #suroeste
                 if x<6 and y>1:
-                    if matriz[x+1][y-1]==op(turno):
-                        # print("suroeste")
-                        a=x+2
-                        b=y-2
-                        while a<=7 and b>=0:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a+1
-                            b=b-1
+                    suroeste(matriz, mov, turno, x, y)
                 #sureste
                 if x<6 and y<6:
-                    if matriz[x+1][y+1]==op(turno):
-                        # print("sureste")
-                        a=x+2
-                        b=y+2
-                        while a<=7 and b<=7:
-                            if matriz[a][b]==2:
-                                mov.append(str(a)+str(b))
-                                break
-                            elif matriz[a][b]==turno:
-                                break
-                            a=a+1
-                            b=b+1
+                    sureste(matriz, mov, turno, x, y)
+
     max = len(mov)-1
+    indice = BusquedaPorAdversario(max, mov)
+
+    return render_template("index.html",
+                           mycontent=str(mov[indice]))
+
+def getTurno(turno):
+    return 1 if turno == 0 else 0
+
+def norte(matriz, mov, turno, x, y):
+    if matriz[x - 1][y] == getTurno(turno):
+        # print("norte")
+        a = x - 2
+        while a >= 0:
+            if matriz[a][y] == 2:
+                mov.append(str(a) + str(y))
+                break
+            elif matriz[a][y] == turno:
+                break
+            a = a - 1
+
+def sur(matriz, mov, turno, x, y):
+    if matriz[x + 1][y] == getTurno(turno):
+        # print("sur")
+        a = x + 2
+        while a <= 7:
+            if matriz[a][y] == 2:
+                mov.append(str(a) + str(y))
+                break
+            elif matriz[a][y] == turno:
+                break
+            a = a + 1
+
+def oeste(matriz, mov, turno, x, y):
+    if matriz[x][y - 1] == getTurno(turno):
+        # print("oeste")
+        a = y - 2
+        while a >= 0:
+            if matriz[x][a] == 2:
+                mov.append(str(x) + str(a))
+                break
+            elif matriz[x][a] == turno:
+                break
+            a = a - 1
+
+def este(matriz, mov, turno, x, y):
+    if matriz[x][y + 1] == getTurno(turno):
+        # print("este")
+        a = y + 2
+        while a <= 7:
+            if matriz[x][a] == 2:
+                mov.append(str(x) + str(a))
+                break
+            elif matriz[x][a] == turno:
+                break
+            a = a + 1
+
+def noroeste(matriz, mov, turno, x, y):
+    if matriz[x - 1][y - 1] == getTurno(turno):
+        # print("noroeste")
+        a = x - 2
+        b = y - 2
+        while a >= 0 and b >= 0:
+            if matriz[a][b] == 2:
+                mov.append(str(a) + str(b))
+                break
+            elif matriz[a][b] == turno:
+                break
+            a = a - 1
+            b = b - 1
+
+def noreste(matriz, mov, turno, x, y):
+    if matriz[x - 1][y + 1] == getTurno(turno):
+        # print("noreste")
+        a = x - 2
+        b = y + 2
+        while a >= 0 and b <= 7:
+            if matriz[a][b] == 2:
+                mov.append(str(a) + str(b))
+                break
+            elif matriz[a][b] == turno:
+                break
+            a = a - 1
+            b = b + 1
+
+def suroeste(matriz, mov, turno, x, y):
+    if matriz[x + 1][y - 1] == getTurno(turno):
+        # print("suroeste")
+        a = x + 2
+        b = y - 2
+        while a <= 7 and b >= 0:
+            if matriz[a][b] == 2:
+                mov.append(str(a) + str(b))
+                break
+            elif matriz[a][b] == turno:
+                break
+            a = a + 1
+            b = b - 1
+
+def sureste(matriz, mov, turno, x, y):
+    if matriz[x + 1][y + 1] == getTurno(turno):
+        # print("sureste")
+        a = x + 2
+        b = y + 2
+        while a <= 7 and b <= 7:
+            if matriz[a][b] == 2:
+                mov.append(str(a) + str(b))
+                break
+            elif matriz[a][b] == turno:
+                break
+            a = a + 1
+            b = b + 1
+
+def BusquedaPorAdversario(max, mov):
     heuristica = [
-        [120, -20, 20, 5, 5, 20, -20, 120],
+        [120, -20, 20,  5,  5, 20, -20, 120],
         [-20, -40, -5, -5, -5, -5, -40, -20],
-        [20, -5, 15, 3, 3, 15, -5, 20],
-        [5, -5, 3, 3, 3, 3, -5, 5],
-        [5, -5, 3, 3, 3, 3, -5, 5],
-        [20, -5, 15, 3, 3, 15, -5, 20],
+        [ 20,  -5, 15,  3,  3, 15,  -5,  20],
+        [  5,  -5,  3,  3,  3,  3,  -5,   5],
+        [  5,  -5,  3,  3,  3,  3,  -5,   5],
+        [ 20,  -5, 15,  3,  3, 15,  -5,  20],
         [-20, -40, -5, -5, -5, -5, -40, -20],
-        [120, -20, 20, 5, 5, 20, -20, 120]
+        [120, -20, 20,  5,  5, 20, -20, 120]
     ]
-    valor = []
+
+    valor = {}
     a=0
     while a<=max:
         x=int(mov[a][0])
         y=int(mov[a][1])
-        valor.append(heuristica[x][y])
+        valor[a]=heuristica[x][y]
         a=a+1
 
-    mayor = 0
-    maxs = []
-    b = 0
-    while b<len(valor):
-        if valor[b] >= mayor:
-            mayor = valor[b]
-            maxs.append(b)
-        b=b+1
-
-    print(mov[maxs[0]])
-    return str(mov[maxs[0]])
+    # ordenar por maximo valor
+    list_sort = sorted(valor.items(), key=operator.itemgetter(1), reverse=True)
+    tuple_result = list_sort[0]
+    indice = tuple_result[0]
+    return indice
